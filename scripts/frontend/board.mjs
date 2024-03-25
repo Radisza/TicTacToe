a9c59c6d17dff6017e3790d1a862129b69135aaf
Ela <radisza5@gmail.com>
2024-03-25 07:50:43 +0100

    Separate js file into modules
    Add app.py file to start server using flask
    Otherwise separated files are not working

---
+++ scripts/frontend/board.mjs
@@ -0,0 +1,46 @@
+export function createBoard(size) {
+    // const board = Array.from({length: size}, () => Array.from({length: size}));
+
+    const get_cell_coordinates = (cell_id) =>  {
+        let result = cell_id.match(/board\(([0-9]+), ([0-9]+)\)/);
+        if (!result) {
+            console.log("Invalid cell id. Expected: board(x, y), got: " +  cell_id);
+        }
+
+        return [result[1], result[2]];
+    }
+
+    const set_hover_effect = (cell) => {
+        cell.addEventListener('mouseover', (event) => {
+            event.target.setAttribute('style', 'background-color: #cffafe')
+        })
+        cell.addEventListener('mouseout', (event) => {
+            event.target.setAttribute('style', 'background-color: ')
+        })
+    }
+
+    const set_click_event = (cell) => {
+        cell.addEventListener('click', (event) => {
+            let [x, y] = get_cell_coordinates(event.target.id);
+            console.log(`Clicked (${x}, ${y})`);
+        });
+    }
+
+    const create_html_elem = () => {
+        const table = document.createElement('table');
+        for (let i = 0 ; i < size; i++) {
+            const row = table.insertRow(-1);
+            for (let j = 0; j < size; j++) {
+                const cell = row.insertCell(-1);
+
+                cell.setAttribute('id', `board(${i}, ${j})`);
+
+                set_click_event(cell);
+                set_hover_effect(cell);
+            }
+        }
+        return table;
+    }
+
+    return {create_html_elem};
+}
