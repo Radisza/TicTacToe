0d0cb2ec3982a7f5c5dee54f017214a4b51bf0b4
Ela <radisza5@gmail.com>
2024-03-27 07:31:34 +0100

    Clean frontend board code

--- scripts/frontend/board.mjs
+++ scripts/frontend/board.mjs
@@ -1,6 +1,8 @@
 
 export function createBoard(size) {
 
+    const get_board_id = (row, col) => `board(${row}, ${col})`;
+
     const get_cell_coordinates = (cell_id) =>  {
         let result = cell_id.match(/board\(([0-9]+), ([0-9]+)\)/);
         if (!result) {
@@ -10,34 +12,16 @@ export function createBoard(size) {
         return [result[1], result[2]];
     }
 
-    const set_hover_effect = (cell) => {
-        cell.addEventListener('mouseover', (event) => {
-            event.target.setAttribute('style', 'background-color: #cffafe')
-        })
-        cell.addEventListener('mouseout', (event) => {
-            event.target.setAttribute('style', 'background-color: ')
-        })
-    }
-
-    const set_click_event = (cell) => {
-        cell.addEventListener('click', (event) => {
-            let [x, y] = get_cell_coordinates(event.target.id);
-            console.log(`Clicked (${x}, ${y})`);
-        });
-    }
-
     const create_html_elem = () => {
         const table = document.createElement('table');
         for (let i = 0 ; i < size; i++) {
             const row = table.insertRow(-1);
             for (let j = 0; j < size; j++) {
                 const cell = row.insertCell(-1);
-                cell.setAttribute('id', `board(${i}, ${j})`);
+                cell.setAttribute('id', get_board_id(i, j));
             }
         }
 
-        set_click_event(table);
-        set_hover_effect(table);
         return table;
     }
 
