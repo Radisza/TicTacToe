0fd7a3d938ab4d86b5f66c7ddb08226daeb952c6
Ela <radisza5@gmail.com>
2024-03-27 07:37:46 +0100

    Add hover and click events in the screen controller

--- scripts/frontend/screenController.mjs
+++ scripts/frontend/screenController.mjs
@@ -14,11 +14,32 @@ export function createScreenController(board_size) {
         board_container.appendChild(board.get_html_elem());
     }
 
+    const set_hover_effect = (table) => {
+        table.addEventListener('mouseover', (event) => {
+            event.target.setAttribute('style', 'background-color: #cffafe')
+        })
+        table.addEventListener('mouseout', (event) => {
+            event.target.setAttribute('style', 'background-color: ')
+        })
+    }
+
+    const set_click_event = (table) => {
+        table.addEventListener('click', (event) => {
+            let [x, y] = board.get_cell_coordinates(event.target.id);
+
+            console.log(`Clicked (${x}, ${y})`);
+            console.log(event.target);
+        });
+    }
+
     let game = createGame(board_size);
     const board = createBoard(board_size);
     const players = Object.fromEntries(
         game.get_players().map(
             (player) => [player.get_symbol(), createPlayerStats(player)]));
+
+    set_click_event(board.get_html_elem());
+    set_hover_effect(board.get_html_elem());
 
 
     new_game();
